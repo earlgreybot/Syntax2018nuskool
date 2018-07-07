26d2027500ae506d3a5b597b48e99e227152de82
Nathan G <gellienathan@hotmail.com.au>
2018-07-07 15:34:06 +1000

    added bees

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,22 @@
 console.log('this is valig code');
+
+// requests to open file
+var bee = new XMLHttpRequest();
+var beeText;
+// paragraph data to change
+var paragraph = document.querySelector
+// opens the file, selects location, yes password all good
+bee.open("GET", 'res/notbee.txt', true);
+
+bee.onload = function() {
+    // if the bee has in fact loaded
+    if (bee.readyState === bee.DONE) {
+        // all content loaded when readystate is done and status is 200
+        if (bee.status === 200) {
+            console.log(bee.response);
+            console.log(bee.responseText);
+        }
+
+    }
+}
+bee.send(null);
